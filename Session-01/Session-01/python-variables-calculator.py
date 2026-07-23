09ba0116b6eac9bafac4f47994759ff41ef8824a
Maryam Ibrahim <306017564+maryamibrahim-dev@users.noreply.github.com>
2026-07-23 16:25:31 +0300

    Add Python Variables Calculator task
    Implemented basic calculator operations as part of the Python Programming Fundamentals Bootcamp

---
+++ Session-01/Session-01/python-variables-calculator.py
@@ -0,0 +1,23 @@
+# Task 1 - python variables calculator
+num1 = 10
+num2 = 5
+print("="*30)
+
+# addition (+)
+num3 = num1 + num2
+print(num3)
+print("="*30)
+
+# subtraction (-)
+num4 = num1 - num2
+print(num4)
+print("="*30)
+
+# multiplication (*)
+num5 = num1 * num2
+print(num5)
+print("="*30)
+
+# division (/)
+num6 = num1 / num2
+print(num6)
