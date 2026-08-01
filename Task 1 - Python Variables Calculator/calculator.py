a6738303e81a277fee8b63bccb21fdaf67a835a2
Maryam Ibrahim <306017564+maryamibrahim-dev@users.noreply.github.com>
2026-08-02 02:07:05 +0300

    Add Task 1 Python Variables Calculator
    Implement basic arithmetic operations: addition, subtraction, multiplication, and division.

---
+++ Task 1 - Python Variables Calculator/calculator.py
@@ -0,0 +1,24 @@
+print("==============Task1==================")
+print("=========Python Variables============")
+print("===============Calculator============")
+num1 = 10
+num2 = 5
+
+# addition (+)
+num3 = num1 + num2
+print(num3)
+
+print("===============================")
+# subtraction (-)
+num4 = num1 - num2
+print(num4)
+
+print("===============================")
+# multiplication (*)
+num5 = num1 * num2
+print(num5)
+
+print("===============================")
+# division (/)
+num6 = num1 / num2
+print(num6)
